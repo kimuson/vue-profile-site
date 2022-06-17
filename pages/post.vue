d44538dd31f77343bf9d8779dc9ef08ae6b12a3a
kimuson <kimurayut6677@gmail.com>
2022-06-18 08:39:03 +0900

    add posts page

--- pages/post.vue
+++ pages/post.vue
@@ -1,13 +1,118 @@
 <template>
   <div id="post">
-     <p>post</p>
+     <h2 class="pt-5 pb-5">Popular Posts</h2>
+      <v-row class="pb-5">
+          <v-col
+            cols="12"
+            sm="6"
+          >
+          <div >
+             <a href="https://kimuralog.com/?p=2420">
+            <v-img
+            contain
+            lazy-src="https://picsum.photos/id/11/10/6"
+            src="https://kimuralog.com/wp-content/uploads/2022/05/5%E5%88%86%E3%81%A6%E3%82%99%E3%82%8F%E3%81%8B%E3%82%8B%EF%BC%81-Flutter%E5%85%A5%E9%96%80-1.jpg"
+            class="pa-3 ma-3 img-circle"
+            ></v-img></a>
+            <p class="pa-1 ma-1">5分でわかるFlutter入門</p>            
+          </div>
+          </v-col>
+          <v-col
+            cols="12"
+            sm="6"
+          >
+         <div>
+             <a href="https://kimuralog.com/?p=2374">
+            <v-img
+            contain
+            lazy-src="https://picsum.photos/id/11/10/6"
+            src="https://kimuralog.com/wp-content/uploads/2022/05/%E3%80%9013%E3%83%B6%E6%9C%88%E7%9B%AE%E3%80%91-%E6%8A%80%E8%A1%93%E3%83%95%E3%82%99%E3%83%AD%E3%82%AF%E3%82%99%E3%81%AE-%E3%82%A2%E3%82%AF%E3%82%BB%E3%82%B9%E3%81%A8%E5%8F%8E%E5%85%A5%E5%85%AC%E9%96%8B.jpg"
+            class="pa-3 ma-3 img-circle"
+            ></v-img></a>
+            <p class="pa-1 ma-1">【13ヶ月目】技術ブログのアクセスと収入公開</p>            
+          </div>
+         </v-col>
+         <v-col
+            cols="12"
+            sm="6"
+          >
+         <div>
+             <a href="https://kimuralog.com/?p=355">
+            <v-img
+            contain
+            lazy-src="https://picsum.photos/id/11/10/6"
+            src="https://kimuralog.com/wp-content/uploads/2022/04/%E3%80%90%E5%85%A5%E9%96%80%E8%80%85%E5%90%91%E3%81%91%E3%80%91-AI%E3%83%BB%E6%A9%9F%E6%A2%B0%E5%AD%A6%E7%BF%92%E3%82%92%E5%AD%A6%E3%81%B5%E3%82%99%E3%81%9F%E3%82%81%E3%81%AE-%E3%81%8A%E3%81%99%E3%81%99%E3%82%81%E6%9B%B8%E7%B1%8D-8%E9%81%B8-1.png"
+            class="pa-3 ma-3 img-circle"
+            ></v-img></a>
+            <p class="pa-1 ma-1">【入門】AI・機械学習を学ぶためのおすすめ書籍 12選</p>            
+          </div>
+         </v-col>
+         <v-col
+            cols="12"
+            sm="6"
+          >
+         <div>
+             <a href="https://kimuralog.com/?p=1784">
+            <v-img
+            contain
+            lazy-src="https://picsum.photos/id/11/10/6"
+            src="https://kimuralog.com/wp-content/uploads/2022/02/The-Internet-of-Things.png"
+            class="pa-3 ma-3 img-circle"
+            ></v-img></a>
+            <p class="pa-1 ma-1">【入門】Swift学習におすすめのUdemy教材3選</p>            
+          </div>
+         </v-col>
+         <v-col
+            cols="12"
+            sm="6"
+          >
+         <div>
+             <a href="https://kimuralog.com/?p=1456">
+            <v-img
+            contain
+            lazy-src="https://picsum.photos/id/11/10/6"
+            src="https://kimuralog.com/wp-content/uploads/2021/08/%E6%97%A5%E6%9C%AC%E8%AA%9E%E3%81%A6%E3%82%99Flutter%E3%81%AB%E5%85%A5%E9%96%80%E3%81%99%E3%82%8B%E5%8B%89%E5%BC%B7%E6%96%B9%E6%B3%95.png"
+            class="pa-3 ma-3 img-circle"
+            ></v-img></a>
+            <p class="pa-1 ma-1">日本語でFlutterに入門する勉強方法</p>            
+          </div>
+         </v-col>
+         <v-col
+            cols="12"
+            sm="6"
+          >
+         <div>
+             <a href="https://kimuralog.com/?p=1569">
+            <v-img
+            contain
+            lazy-src="https://picsum.photos/id/11/10/6"
+            src="https://kimuralog.com/wp-content/uploads/2021/09/iPhone55Display.001-1-2048x1152.jpeg"
+            class="pa-3 ma-3 img-circle"
+            ></v-img></a>
+            <p class="pa-1 ma-1">【アップデートのお知らせ】MarkOne ver8.2.1がリリースされました🎉</p>            
+          </div>
+         </v-col>
+        </v-row>
   </div>
 </template>
  
 <script>
 
-
 export default {
   name: 'post'
 }
 </script>
+
+<style>
+
+
+.sample {
+   white-space:nowrap;
+   overflow:hidden;
+   text-overflow:ellipsis;
+}
+.img-circle {
+    border-radius: 10%;
+}
+
+</style>
--- pages/work.vue
+++ pages/work.vue
@@ -1,38 +1,103 @@
 <template>
   <div id="work">
-     <p>Works</p>
-      <v-row>
+     <h2 class="pt-5 pb-5">Works</h2>
+      <v-row class="pb-5">
           <v-col
             cols="12"
             sm="6"
           >
           <div>
-            <p>コンテンツタイトル</p>
-            <p>画像</p>
-            <p>説明</p>
-          </div>
-          <div>
-            <p>コンテンツタイトル</p>
-            <p>画像</p>
-            <p>説明</p>
+            <h3>MarkOne / 2021</h3>
+            <v-img
+            contain
+            lazy-src="https://picsum.photos/id/11/10/6"
+            max-height="125"
+            src="https://is2-ssl.mzstatic.com/image/thumb/PurpleSource126/v4/f9/22/a9/f922a9f0-e271-6483-f63f-b68232dcf06a/7121f7e4-2755-42cc-bf18-e735dadee63f_iPhone65Display.001.jpeg/626x0w.webp"
+            class="pa-3 ma-3"
+            ></v-img>
+            <p class="pa-1 ma-1">MarkOne is an iOS application that automatically collects articles from major web media.</p>
+
+            <v-row justify="center" class="pa-3">
+               <v-btn color="primary" text href="https://kimuralog.com/?p=60">Detail ></v-btn>
+            </v-row>
+            
           </div>
           </v-col>
           <v-col
             cols="12"
             sm="6"
           >
-          <div>
-            <p>コンテンツタイトル</p>
-            <p>画像</p>
-            <p>説明</p>
-          </div>
-          <div>
-            <p>コンテンツタイトル</p>
-            <p>画像</p>
-            <p>説明</p>
-          </div>
-          </v-col>
+         <div>
+            <h3>Peep Blocker / 2019</h3>
+            <v-img
+            contain
+            class="pa-3 ma-3"
+            lazy-src="https://picsum.photos/id/11/10/6"
+            max-height="125"
+            src="https://kimuralog.com/wp-content/uploads/2021/09/demo.gif"
+            ></v-img>
+            <p class="pa-1 ma-1">It is a peep prevention system for home work and telework.Praying for the convergence of the coronas.</p>
+            <v-row justify="center" class="pa-3">
+               <v-btn color="primary" text href="https://kimuralog.com/?p=60">Detail ></v-btn>
+            </v-row>
+         </div>
+         </v-col>
+         <v-col
+            cols="12"
+            sm="6"
+          >
+         <div>
+            <h3>Twitter Bot 'Banana Husband' / 2018</h3>
+            <v-img
+            contain
+            class="pa-3 ma-3"
+            lazy-src="https://picsum.photos/id/11/10/6"
+            max-height="125"
+            src="https://kimuralog.com/wp-content/uploads/2021/09/Screen-Shot-2021-09-18-at-16.31.39-1024x757.png"
+            ></v-img>
+            <p class="pa-1 ma-1">This bot automatically collects and tweets IT news. It also automatically returns comments.</p>
+            <v-row justify="center" class="pa-3">
+               <v-btn color="primary" text href="https://kimuralog.com/?p=60">Detail ></v-btn>
+            </v-row> 
+         </div>
+         </v-col>
         </v-row>
+
+      <v-divider></v-divider>
+      <h2 class="pt-5 pb-5">Collaborations</h2>
+       <v-row class="pb-5">
+          <v-col
+            cols="12"
+            sm="6"
+          >
+          <div>
+            <h3>SmartBiz合同会社様 Website Construction / 2020</h3>
+            <v-img
+            contain
+            lazy-src="https://picsum.photos/id/11/10/6"
+            max-height="125"
+            src="https://kimuralog.com/wp-content/uploads/2021/09/Screen-Shot-2021-09-18-at-16.29.13-1024x765.png"
+            class="pa-3 ma-3"
+            ></v-img>
+          </div>
+          </v-col>
+          <v-col
+            cols="12"
+            sm="6"
+          >
+         <div>
+            <h3>株式会社スカインド様 Website Construction / 2020</h3>
+            <v-img
+            contain
+            class="pa-3 ma-3"
+            lazy-src="https://picsum.photos/id/11/10/6"
+            max-height="125"
+            src="https://kimuralog.com/wp-content/uploads/2021/09/Screen-Shot-2021-09-18-at-16.28.46-1024x800.png"
+            ></v-img>
+         </div>
+         </v-col>
+        </v-row>
+
   </div>
 </template>
  
